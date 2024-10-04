087b6828af09f5b7c1f9639829f49f40c3aa2eef
Nagi779 <jluo@macalester.edu>
2024-10-03 20:05:03 -0500

    make a list of cells

--- src/cellabsorption/CellSimulation.java
+++ src/cellabsorption/CellSimulation.java
@@ -5,11 +5,13 @@ import edu.macalester.graphics.Ellipse;
 import edu.macalester.graphics.Point;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @SuppressWarnings("SameParameterValue")
 public class CellSimulation {
-    private Cell cell;
+    private List<Cell> cells;
     private CanvasWindow canvas;
     private Random rand = new Random();
 
@@ -25,8 +27,10 @@ public class CellSimulation {
         //noinspection InfiniteLoopStatement
         while (true) {
             Point canvasCenter = new Point(canvas.getWidth() / 2.0, canvas.getHeight() / 2.0);
+            for (Cell cell : cells) {
             cell.moveAround(canvasCenter);
             cell.grow(0.02);
+            }
 
             canvas.draw();
             canvas.pause(10);
@@ -34,14 +38,18 @@ public class CellSimulation {
     }
 
     private void populateCells() {
-        double size = rand.nextInt(5) + 2;
-        cell = new Cell(
-            rand.nextDouble() * (canvas.getWidth() - size),
-            rand.nextDouble() * (canvas.getHeight() - size),
-            size,
-            Color.getHSBColor(rand.nextFloat(), rand.nextFloat() * 0.5f + 0.1f, 1)
-        );
+        cells = new ArrayList<>();
+        for (int i = 0; i < 200; i++) {  
+            double size = rand.nextInt(5) + 2;
+            Cell cell = new Cell(
+                rand.nextDouble() * (canvas.getWidth() - size),
+                rand.nextDouble() * (canvas.getHeight() - size),
+                size,
+                Color.getHSBColor(rand.nextFloat(), rand.nextFloat() * 0.5f + 0.1f, 1)
+            );
         canvas.add(cell.getShape());
+        cells.add(cell); 
+        }
     }
 }
 
